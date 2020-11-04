f25904316d672fe31ae607a7a14fb895b2b06cc7
Zadok Ashitei <zadokarmah1996@gmail.com>
2020-11-04 09:14:22 +0000

    import os and sys

--- database_setup.py
+++ database_setup.py
@@ -1,13 +1,15 @@
+import os
+import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
@@ -18,7 +20,7 @@ class Restaurant(Base):
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -46,6 +48,6 @@ class MenuItem(Base):
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
- 
+
 
 Base.metadata.create_all(engine)
